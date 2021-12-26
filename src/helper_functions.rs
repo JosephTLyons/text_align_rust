9e5504c2a16e96681a53bce71586373c91d77e9c
Joseph T Lyons <JosephTLyons@gmail.com>
2021-12-26 17:28:33 -0500

    Finish get_index_spread()

--- src/helper_functions.rs
+++ src/helper_functions.rs
@@ -1,28 +1,26 @@
 use regex::Regex;
 use std::fmt;
 
-pub fn get_evenly_seleted_indices(items_count: usize, distribution_value: usize) -> Vec<usize> {
-    let none_number = items_count - distribution_value;
-    let mut indices: Vec<Option<usize>> = (0..items_count).map(Some).collect();
+// A modified version of: https://stackoverflow.com/a/9873804
+pub fn get_index_spread(size: usize, n: usize) -> Option<Vec<usize>> {
+    if size == 0 || n == 0 {
+        return None;
+    }
 
+    let indices: Vec<usize> = if n >= size {
+        (0..size).collect()
+    } else {
+        (0..n)
+            .map(|i| {
+                let a = (i * size) / n;
+                let b = size / (2 * n);
+                a + b
+            })
+            .collect()
+    };
 
-
-    let indices: Vec<usize> = indices.into_iter().flatten().collect();
-
-    indices
+    Some(indices)
 }
-
-// [1, 0, 1, 0, 1, 0, 0, 0]
-// 3 -> 8 / 3 = 2
-
-// [1, 0, 1, 0, 1, 0, 0, 0]
-// 3 -> 8 / 3 = 2
-
-// [1, 0, 1, 0, 1, 0, 1, 0]
-// 5 -> 4
-
-// [Some(0), None, Some(2), None, Some(4), Some(5), Some(6), Some(7), None]
-// [0, 2, 4, 5, 6, 7]
 
 pub fn replace_matches<T: AsRef<str> + fmt::Display>(
     text: &T,
@@ -39,14 +37,78 @@ pub fn replace_matches<T: AsRef<str> + fmt::Display>(
 mod tests {
     use super::*;
 
-    // #[test]
-    fn test_get_evenly_seleted_indices() {
-        // [0, 0, 0, 0, 0, 0, 0, 0];
-        let output = get_evenly_seleted_indices(8, 3);
-        assert_eq!(output, [0, 3, 6]);
+    #[test]
+    fn test_get_index_spread_special_cases() {
+        let output = get_index_spread(1, 0);
+        assert_eq!(output, None);
 
-        // let output = get_evenly_seleted_indices(&items, 5);
-        // assert_eq!(output, [1, 0, 0, 1, 0, 0, 1, 0])
+        let output = get_index_spread(0, 1);
+        assert_eq!(output, None);
+
+        let output = get_index_spread(8, 100);
+        assert_eq!(output.unwrap(), [0, 1, 2, 3, 4, 5, 6, 7]);
+    }
+
+    #[test]
+    fn test_get_index_spread_even_size() {
+        // 0 -> [o, o, o, o, o, o, o, o]
+        // 1 -> [o, o, o, o, |, o, o, o]
+        // 2 -> [o, o, |, o, o, o, |, o]
+        // 3 -> [o, |, o, |, o, o, |, o]
+        // 4 -> [o, |, o, |, o, |, o, |]
+        // 5 -> [|, |, o, |, |, o, |, o]
+        // 6 -> [|, |, |, o, |, |, |, o]
+        // 7 -> [|, |, |, |, |, |, |, o]
+        // 8 -> [|, |, |, |, |, |, |, |]
+
+        let output = get_index_spread(8, 1);
+        assert_eq!(output.unwrap(), [4]);
+
+        let output = get_index_spread(8, 2);
+        assert_eq!(output.unwrap(), [2, 6]);
+
+        let output = get_index_spread(8, 3);
+        assert_eq!(output.unwrap(), [1, 3, 6]);
+
+        let output = get_index_spread(8, 4);
+        assert_eq!(output.unwrap(), [1, 3, 5, 7]);
+
+        let output = get_index_spread(8, 5);
+        assert_eq!(output.unwrap(), [0, 1, 3, 4, 6]);
+
+        let output = get_index_spread(8, 6);
+        assert_eq!(output.unwrap(), [0, 1, 2, 4, 5, 6]);
+
+        let output = get_index_spread(8, 7);
+        assert_eq!(output.unwrap(), [0, 1, 2, 3, 4, 5, 6]);
+
+        let output = get_index_spread(8, 8);
+        assert_eq!(output.unwrap(), [0, 1, 2, 3, 4, 5, 6, 7]);
+    }
+
+    #[test]
+    fn test_get_index_spread_odd_size() {
+        // 0 -> [o, o, o, o, o]
+        // 1 -> [o, o, |, o, o]
+        // 2 -> [o, |, o, |, o]
+        // 3 -> [|, |, o, |, o]
+        // 4 -> [|, |, |, |, o]
+        // 5 -> [|, |, |, |, |]
+
+        let output = get_index_spread(5, 1);
+        assert_eq!(output.unwrap(), [2]);
+
+        let output = get_index_spread(5, 2);
+        assert_eq!(output.unwrap(), [1, 3]);
+
+        let output = get_index_spread(5, 3);
+        assert_eq!(output.unwrap(), [0, 1, 3]);
+
+        let output = get_index_spread(5, 4);
+        assert_eq!(output.unwrap(), [0, 1, 2, 3]);
+
+        let output = get_index_spread(5, 5);
+        assert_eq!(output.unwrap(), [0, 1, 2, 3, 4]);
     }
 
     #[test]
